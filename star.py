5183aa6aab1a92d9cffaf582c8024d15c437986e
January-cats <januarycats09@gmail.com>
2021-08-05 00:40:53 +0900

    issue2

--- star.py
+++ star.py
@@ -6,6 +6,7 @@ import sys
 from pygame.locals import QUIT, Rect, KEYDOWN,  K_w, K_a, K_s, K_d, K_SPACE, K_h
 from random import randint
 import copy
+import csv
 
 from Entity import Entity
 
@@ -252,7 +253,7 @@ class FieldPart(Entity):
         w = en.width
         h = en.height
         r = ((x >= self.x and x <= self.x + self.width)or\
-        (x<self.x and self.x < x + w)) and \
+        (x < self.x and self.x < x + w)) and \
         ((y >= self.y and y <= self.y + self.height)or\
         (y < self.y and self.y < y+h))
         return r
@@ -263,28 +264,74 @@ class Field():
     DWIDTH = 10 #分割された長方形の幅
     #FieldPartをまとめたクラス
     def __init__(self):
-        self.holes = [] # 現在画面に表示する地形を格納する、上のパーツと下のパーツに分けて配置（リスト）
-        self.field = [] # あらかじめ設定されたフィールドを格納する
-        for i in range(Field.WALL+1): #横に並んだ長方形で初期化
+        self.blocks = [] # 現在画面に表示する地形を格納する、上のパーツと下のパーツに分けて配置（リスト）
+        self.field = [] # あらかじめ設定された地形情報を格納する、[上, 下]の二次元リスト
+        self.progress = 0 #進んだFieldPartの数
+        for i in range(Field.WALL+1): #横に並んだ長方形で初期化、上下それぞれ、スクロール用に右に余分に一つ作る
             upper = FieldPart(i*10, 0, Field.DWIDTH, 50)
             lower = FieldPart(i*10, 550, Field.DWIDTH, 50)
-            self.holes.append([upper, lower])
+            self.blocks.append([upper, lower])
 
     def disp(self):
         SURFACE.fill((0, 0, 0)) #廃液を黒で染める
-        for hole in self.holes: #地形の場所を緑で表示
-            hole[0].disp() #FieldPartクラスのdisp()
-            hole[1].disp()
+        for block in self.blocks: #地形の場所を緑で表示
+            block[0].disp() #FieldPartクラスのdisp()
+            block[1].disp()
 
     def collision(self, entity):
-        #entityと、holesのなかのentityが一つでも接触しているかどうかで判定する
+        #entityと、blocksのなかのentityが一つでも接触しているかどうかで判定する
         r = False
-        for hole in self.holes:
+        for block in self.blocks:
             #上もしくは下の地形に接触しているかを確認
-            if hole[0].collision(entity) or hole[1].collision(entity):
+            if block[0].collision(entity) or block[1].collision(entity):
                 r = True
                 break
         return r
+
+    def load(self, filename):
+        #csvで記述された地形情報を読み込む
+        with open(filename, 'r') as f: #ファイルを読み込みモードで開く
+            header = next(csv.reader(f)) #ヘッダー部
+            reader = csv.reader(f) #データ部
+            field = [] #仮の配列
+            for row in reader:
+                field.append([int(row[0]), int(row[1])])
+            if len(field) < 31:
+                return
+            else:
+                #地形情報を追加
+                self.field = field
+                self.field.append('EOF') #リストの最後にEOF
+        return
+
+    def initialize(self):
+        #地形を初期状態にする
+        for i in range(Field.WALL+1):
+            #地形情報を下にブロックを作成する
+            upper = FieldPart(i*10, 0, Field.DWIDTH, self.field[i][0])
+            lower = FieldPart(i*10, self.field[i][1], Field.DWIDTH, 600 - self.field[i][1])
+            self.blocks[i] = [upper, lower]
+        return
+
+    def scroll(self):
+        #地形を左に動かす
+        #読み込んだ右端まで言ったら止まるようにしたい
+        a = 0
+        if self.field[self.progress + Field.WALL] != 'EOF':
+            #地形の終わりじゃないなら左に動かす
+            for block in self.blocks:
+                block[0].move()
+                block[1].move() #上下のブロックを左に動かす
+            if self.blocks[0][0].x + Field.DWIDTH <= 0: #一番左側のブロックが画面右側へ消えたら
+                for i in range(Field.WALL):
+                    self.blocks[i] = self.blocks[i+1] #ブロックを左寄せ
+                if self.field[self.progress + Field.WALL + 1] != 'EOF':
+                    #新しい分のブロックを追加
+                    upper = FieldPart(800, 0, Field.DWIDTH, self.field[self.progress + Field.WALL + 1][0])
+                    lower = FieldPart(800, self.field[self.progress + Field.WALL + 1][1], Field.DWIDTH, 600-self.field[self.progress + Field.WALL + 1][1])
+                    self.blocks[Field.WALL] = [upper, lower]
+                self.progress += 1
+        return
 
 
 class BarWrapper():
@@ -337,7 +384,6 @@ def main():
     bar_wrapper = BarWrapper()
     machinegun_bar = Bar(20, 20, 100 ,20)
     missile_bar = Bar(20, 60, 100, 20)
-    field = Field()
     buds = [] #敵の一覧を保持するリスト
     stars = []
     bullets = []
@@ -353,6 +399,11 @@ def main():
     #表示するゲージをリストに追加
     bar_wrapper.add(missile_bar)
     bar_wrapper.add(machinegun_bar)
+
+    #地形の初期化
+    field = Field()
+    field.load("ftest.csv") #地形情報の読み込み
+    field.initialize()
 
 
     while True:
@@ -418,6 +469,9 @@ def main():
         for bud in buds:
             bud.move(field)
 
+        #地形の移動
+        field.scroll()
+
         #弾と敵の当たり判定
         for n, bullet in enumerate(bullets):
             for bud in buds:
@@ -425,6 +479,11 @@ def main():
                     bud.damage(bullet.get_damege())
                     bud.hit_with(bullet)
                     del bullets[n]
+
+        #弾と地形の当たり判定
+        for n, bullet in enumerate(bullets):
+            if field.collision(bullet):
+                del bullets[n]
 
         #敵の体力が無くなったら消滅させる
         for n, bud in enumerate(buds):
