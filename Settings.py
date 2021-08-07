728715cf50392ad18fcdd9358c38269b379fb41e
January-cats <januarycats09@gmail.com>
2021-08-08 03:13:40 +0900

    update

--- Settings.py
+++ Settings.py
@@ -8,18 +8,38 @@ from pygame.locals import QUIT, Rect, KEYDOWN,  K_w, K_a, K_s, K_d, K_SPACE, K_h
 WIDTH = 800
 HEIGHT = 600 #ゲーム本編を描画するエリア
 INFO_AREA = 100 #ゲージ類を表示するエリア
-FIELD_FILE = "ftest.csv"
+FIELD_FILE = "ftest.csv" #読み込む地形ファイル
 
 #画像ディレクトリのパス
 IMG_DIR = "img/"
 #地形を除く各エンティティのステータス
+SHIP_TYPE = {
+    'TYPE1': {
+        #自機タイプ1
+        'hp': 100,
+        'width': 45,
+        'height': 25,
+        'img': IMG_DIR + "fighter4.png",
+        'mag': [5, 15] #machinegun, missile
+    },
+    'TYPE2': {
+        #自機タイプ2
+        'hp': 100,
+        'width': 50,
+        'height': 20,
+        'img': IMG_DIR + "fighter5.png",
+        'mag': [3, 25] #machinegun, missile
+    }
+}
 FLOATER = {
+    #敵キャラクター
     'hp': 100,
     'width': 40,
     'height': 40,
     'img': IMG_DIR + "floater.png"
 }
 SINKER = {
+    #敵キャラクター
     'hp': 150,
     'width': 50,
     'height': 40,
