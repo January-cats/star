1f12102a08e098a06379687a445f8c1dc8cb56ac
January-cats <januarycats09@gmail.com>
2021-08-19 18:39:22 +0900

    gameover added

--- star.py
+++ star.py
@@ -3,7 +3,7 @@
 
 import pygame
 import sys
-from pygame.locals import QUIT, KEYDOWN,  K_w, K_a, K_s, K_d, K_h, K_SPACE
+from pygame.locals import QUIT, KEYDOWN,  K_w, K_a, K_s, K_d, K_h, K_SPACE, K_r
 
 from Entity import Entity
 from Ship import Ship
@@ -21,12 +21,11 @@ class StarManager():
     #ゲームオーバーなど、ゲームの状態、および各インスタンスを管理する
     def __init__(self):
         #初期化
-        self.game_over = False
         self.ship = Ship(ship_type=1)
 
         #ゲージ
         self.bar_wrapper = BarWrapper()
-        ship_hp_bar = Bar(20, 20, 100 ,20)
+        ship_hp_bar = Bar(20, 20, 200 ,20)
         #表示するゲージをリストに追加
         self.bar_wrapper.add(ship_hp_bar)
 
@@ -72,7 +71,6 @@ class StarManager():
 class Star():
     #ゲーム本体を管理するクラス
     def __init__(self):
-        print("Hello from the star!")
         self.manager = StarManager()
 
     def star(self):
@@ -88,6 +86,8 @@ class Star():
 
         #サブウェポン変更変数
         subweapon_change = False
+        #ゲームオーバー変数
+        game_over = False
 
         keydict = {
             'w': False,
@@ -100,162 +100,187 @@ class Star():
             }
 
         while True:
-            #初期化
-            keydict['w'] = False
-            keydict['a'] = False
-            keydict['s'] = False
-            keydict['d'] = False
-            keydict['SPACE'] = False
-            keydict['ESCAPE'] = False
+            if game_over:
+                #-----------ゲームオーバーになった時の処理--------------
+                SURFACE.fill((0, 0, 0))
+                font = pygame.font.SysFont(None, 36)
+                strimage = font.render("Game Over (Press r to restart)", True, (255, 255, 255))
+                center_str = strimage.get_rect(center=(WIDTH/2, HEIGHT/2))
+                SURFACE.blit(strimage, center_str)
 
-            ship.set_hit_default()
-            for bud in bud_field.get_list():
-                bud.set_hit_default()
+                #イベントキューをかくにん
+                for event in pygame.event.get():
+                    if event.type == KEYDOWN:
+                        if event.key == K_r:
+                            return
+                    if event.type == QUIT:
+                        #ゲームの強制終了
+                        pygame.quit()
+                        sys.exit()
+            else:
+                #初期化
+                keydict['w'] = False
+                keydict['a'] = False
+                keydict['s'] = False
+                keydict['d'] = False
+                keydict['SPACE'] = False
+                keydict['ESCAPE'] = False
 
-            #押されたキーを取得
-            key = pygame.key.get_pressed()
-            if key[K_w]:
-                keydict['w'] = True
-            if key[K_a]:
-                keydict['a'] = True
-            if key[K_s]:
-                keydict['s'] = True
-            if key[K_d]:
-                keydict['d'] = True
-            if key[K_SPACE]:
-                keydict['SPACE'] = True
-            #hキーが押された時に一回だけ動作する
-            if not key[K_h]:
-                keydict['h'] = False
-            elif not keydict['h']:
-                keydict['h'] = True
-                ship.change_subweapon() #武器の切り替え
-            #イベントキューをかくにん
-            for event in pygame.event.get():
-                if event.type == QUIT:
-                    #ゲームの強制終了
-                    pygame.quit()
-                    sys.exit()
-
-            #自機の移動
-            ship.move(keydict)
-
-            #弾の生成
-            if keydict['SPACE']:
-                bul = ship.shoot(type=ship.gun()['MACHINEGUN'])
-                bullet_list.add(bul)
-                if ship.get_subweapon() == 1:
-                    mis = ship.shoot(type=ship.gun()['MISSILE'])
-                    bullet_list.add(mis)
-                if ship.get_subweapon() == 2:
-                    dmis = ship.shoot(type=ship.gun()['DOWN_MISSILE'])
-                    bullet_list.add(dmis)
-            ship.reload()
-
-            #budの射撃
-            for bud in bud_field.get_list():
-                bul = bud.shoot(ship)
-                if bul:
-                    bud_bullet_list.add(bul)
-                bud.reload()
-
-            #星の移動
-            for star in stars:
-                star.move()
-
-            #弾の移動
-            bullet_list.move_all() #自機
-            bud_bullet_list.move_all() #敵
-
-            #敵機の移動
-            bud_field.move_all(field)
-
-            #地形の移動
-            field.scroll()
-
-            #budの出現
-            if field.get_tick() == 0:
-                bud_field.spawn(field.get_progress())
-
-            #-----------当たり判定のチェック----------
-
-            #弾と敵の当たり判定
-            for n, bullet in enumerate(bullet_list.get_list()):
+                ship.set_hit_default()
                 for bud in bud_field.get_list():
-                    if bud.collision(bullet):
-                        bud.damage(bullet.get_damege())
-                        bud.hit_with(bullet)
+                    bud.set_hit_default()
+
+                #押されたキーを取得
+                key = pygame.key.get_pressed()
+                if key[K_w]:
+                    keydict['w'] = True
+                if key[K_a]:
+                    keydict['a'] = True
+                if key[K_s]:
+                    keydict['s'] = True
+                if key[K_d]:
+                    keydict['d'] = True
+                if key[K_SPACE]:
+                    keydict['SPACE'] = True
+                #hキーが押された時に一回だけ動作する
+                if not key[K_h]:
+                    keydict['h'] = False
+                elif not keydict['h']:
+                    keydict['h'] = True
+                    ship.change_subweapon() #武器の切り替え
+                #イベントキューをかくにん
+                for event in pygame.event.get():
+                    if event.type == QUIT:
+                        #ゲームの強制終了
+                        pygame.quit()
+                        sys.exit()
+
+                #自機の移動
+                ship.move(keydict)
+
+                #弾の生成
+                if keydict['SPACE']:
+                    bul = ship.shoot(type=ship.gun()['MACHINEGUN'])
+                    bullet_list.add(bul)
+                    if ship.get_subweapon() == 1:
+                        mis = ship.shoot(type=ship.gun()['MISSILE'])
+                        bullet_list.add(mis)
+                    if ship.get_subweapon() == 2:
+                        dmis = ship.shoot(type=ship.gun()['DOWN_MISSILE'])
+                        bullet_list.add(dmis)
+                ship.reload()
+
+                #budの射撃
+                for bud in bud_field.get_list():
+                    bul = bud.shoot(ship)
+                    if bul:
+                        bud_bullet_list.add(bul)
+                    bud.reload()
+
+                #星の移動
+                for star in stars:
+                    star.move()
+
+                #弾の移動
+                bullet_list.move_all() #自機
+                bud_bullet_list.move_all() #敵
+
+                #敵機の移動
+                bud_field.move_all(field)
+
+                #地形の移動
+                field.scroll()
+
+                #budの出現
+                if field.get_tick() == 0:
+                    bud_field.spawn(field.get_progress())
+
+                #-----------当たり判定のチェック----------
+
+                #弾と敵の当たり判定
+                for n, bullet in enumerate(bullet_list.get_list()):
+                    for bud in bud_field.get_list():
+                        if bud.collision(bullet):
+                            bud.damage(bullet.get_damege())
+                            bud.hit_with(bullet)
+                            bullet_list.delete(n)
+
+                #敵の弾と自機との当たり判定
+                for n, bullet in enumerate(bud_bullet_list.get_list()):
+                    if ship.collision(bullet):
+                        #自機が敵の弾と接触していたら
+                        ship.do_damage(bullet.get_damege())
+                        ship.hit_with(bullet)
+                        bud_bullet_list.delete(n)
+
+                #弾と地形の当たり判定
+                for n, bullet in enumerate(bullet_list.get_list()):
+                    if field.collision(bullet):
                         bullet_list.delete(n)
 
-            #敵の弾と自機との当たり判定
-            for n, bullet in enumerate(bud_bullet_list.get_list()):
-                if ship.collision(bullet):
-                    #自機が敵の弾と接触していたら
-                    ship.do_damage(bullet.get_damege())
-                    ship.hit_with(bullet)
-                    bud_bullet_list.delete(n)
+                #敵の弾と地形の当たり判定
+                for n, bullet in enumerate(bud_bullet_list.get_list()):
+                    if field.collision(bullet):
+                        bud_bullet_list.delete(n)
 
-            #弾と地形の当たり判定
-            for n, bullet in enumerate(bullet_list.get_list()):
-                if field.collision(bullet):
-                    bullet_list.delete(n)
+                #敵と自機の当たり判定
+                for bud in bud_field.get_list():
+                    if ship.collision(bud):
+                        ship.do_damage(1)
+                        ship.hit_with(bud)
 
-            #敵の弾と地形の当たり判定
-            for n, bullet in enumerate(bud_bullet_list.get_list()):
-                if field.collision(bullet):
-                    bud_bullet_list.delete(n)
+                #地形と自機の当たり判定
+                if field.collision(ship):
+                    ship.hit_with()
+                    ship.do_damage(1)
 
-            #敵の体力が無くなったら消滅させる
-            for n, bud in enumerate(bud_field.get_list()):
-                if bud.is_dead():
-                    bud_field.delete(n)
+                #----------体力判定---------------
+                #自機の体力がなくなったらゲームオーバーにする
+                if ship.get_hp() <= 0:
+                    game_over = True
 
-            #敵と自機の当たり判定
-            for bud in bud_field.get_list():
-                if ship.collision(bud):
-                    ship.hit_with(bud)
+                #敵の体力が無くなったら消滅させる
+                for n, bud in enumerate(bud_field.get_list()):
+                    if bud.is_dead():
+                        bud_field.delete(n)
 
-            #地形と自機の当たり判定
-            if field.collision(ship):
-                ship.hit_with()
+                #--------------各要素の描画-----------------
 
-            #--------------各要素の描画-----------------
+                #地形を描画
+                field.disp()
 
-            #地形を描画
-            field.disp()
+                #自機の体力ゲージの描画
+                ship_type = ship.get_type()
+                bar_wrapper.bar_list[0].set((ship.get_hp()/SHIP_TYPE[ship_type]['hp'])*100)
+                bar_wrapper.disp() # ゲージ残量を描画
 
-            #自機の体力ゲージの描画
-            ship_type = ship.get_type()
-            bar_wrapper.bar_list[0].set((ship.get_hp()/SHIP_TYPE[ship_type]['hp'])*100)
-            bar_wrapper.disp() # ゲージ残量を描画
+                #ゲージと画面の境界線
 
-            #ゲージと画面の境界線
+                for star in stars:
+                    star.disp()
+                bud_field.disp_all(hitbox=HITBOX)
+                bullet_list.disp_all()
+                bud_bullet_list.disp_all()
+                ship.disp(hitbox=HITBOX)
 
-            for star in stars:
-                star.disp()
-            bud_field.disp_all(hitbox=HITBOX)
-            bullet_list.disp_all()
-            bud_bullet_list.disp_all()
-            ship.disp(hitbox=HITBOX)
+                #budの体力表示（一時的）
+                font = pygame.font.SysFont(None, 36)
+                s = ''
+                for i in bud_field.get_list():
+                    s += ' {}'.format(i.hp)
+                strimage = font.render("Bud: {}".format(s), True, (255, 255, 255))
+                SURFACE.blit(strimage, (400, 20))
 
-            #budの体力表示（一時的）
-            font = pygame.font.SysFont(None, 36)
-            s = ''
-            for i in bud_field.get_list():
-                s += ' {}'.format(i.hp)
-            strimage = font.render("Bud: {}".format(s), True, (255, 255, 255))
-            SURFACE.blit(strimage, (400, 20))
-
-            #progressとtickを表示
-            font = pygame.font.SysFont(None, 36)
-            strimage = font.render("Progress: {}".format(field.get_progress()), True, (255, 255, 255))
-            SURFACE.blit(strimage, (WIDTH/2, HEIGHT+20))
-            font = pygame.font.SysFont(None, 36)
-            strimage = font.render("tick: {}".format(field.get_tick()), True, (255, 255, 255))
-            SURFACE.blit(strimage, (WIDTH/2, HEIGHT+60))
-            font = pygame.font.SysFont(None, 36)
-            strimage = font.render("weapon: {}".format(ship.get_subweapon()), True, (255, 255, 255))
-            SURFACE.blit(strimage, (WIDTH*3/4, HEIGHT+20))
+                #progressとtickを表示
+                font = pygame.font.SysFont(None, 36)
+                strimage = font.render("Progress: {}".format(field.get_progress()), True, (255, 255, 255))
+                SURFACE.blit(strimage, (WIDTH/2, HEIGHT+20))
+                font = pygame.font.SysFont(None, 36)
+                strimage = font.render("tick: {}".format(field.get_tick()), True, (255, 255, 255))
+                SURFACE.blit(strimage, (WIDTH/2, HEIGHT+60))
+                font = pygame.font.SysFont(None, 36)
+                strimage = font.render("weapon: {}".format(ship.get_subweapon()), True, (255, 255, 255))
+                SURFACE.blit(strimage, (WIDTH*3/4, HEIGHT+20))
 
             #---------画面更新--------------
 
@@ -263,6 +288,7 @@ class Star():
             pygame.display.update()
             FPSCLOCK.tick(FPSTICK) #画面更新は20fps
 
+        return
 
 def main():
     #init pygame
@@ -270,7 +296,10 @@ def main():
     pygame.key.set_repeat(5000, 5)
 
     star = Star()
-    star.star()
+
+    while 1:
+        star.__init__()
+        star.star()
 
     return 0
 
